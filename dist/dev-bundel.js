506a7e3a1940248118c2d8f8d55dbb80372fee1a
Aleksandr <registr99999@gmail.com>
2021-01-14 20:30:04 +0300

    Карточка модального окна

--- dist/dev-bundel.js
+++ dist/dev-bundel.js
@@ -49,7 +49,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _mod
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
 "use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => __WEBPACK_DEFAULT_EXPORT__\n/* harmony export */ });\nvar modalCallback = function modalCallback() {\n  var modalOverlay = document.querySelector('.modal-overlay');\n  var callback = document.getElementById('callback');\n  document.addEventListener('click', function (_ref) {\n    var target = _ref.target;\n\n    if (target.classList.contains('callback-btn')) {\n      modalOverlay.style.display = 'block';\n      callback.style.display = 'block';\n    }\n\n    if (target.classList.contains('.modal-close')) {\n      modalOverlay.style.display = 'none';\n      callback.style.display = 'none';\n    }\n\n    console.log(target);\n  });\n};\n\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (modalCallback);\n\n//# sourceURL=webpack://diplom/./src/modules/modalCallback.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => __WEBPACK_DEFAULT_EXPORT__\n/* harmony export */ });\ndocument.addEventListener('click', function (_ref) {\n  var target = _ref.target;\n  var modalOverlay = document.querySelector('.modal-overlay');\n  var callback = document.getElementById('callback');\n  var feedback = document.getElementById('feedback');\n\n  if (target.closest('.callback-btn')) {\n    modalOverlay.style.cssText = 'display:block; opacity:1; transition: all .9s ease 3s;';\n    callback.style.cssText = 'display:block; opacity:1; transform: scale(1) translate(-50%, 0) rotate(0deg); transition: all .9s ease 1s;';\n  }\n\n  if (target.closest('.modal-overlay') || target.closest('.modal-close')) {\n    modalOverlay.style.cssText = 'display:none; opacity:0; transform:translate(0, 0) rotate(45deg); transition: all .9s ease 3s;';\n    callback.style.cssText = 'display:none; opacity:0; transform:scale(1) translate(0, 0) rotate(45deg); transition: all .9s ease 3s;';\n  }\n});\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (modalCallback);\n\n//# sourceURL=webpack://diplom/./src/modules/modalCallback.js?");
 
 /***/ }),
 
@@ -444,7 +444,7 @@ eval("var map = {\n\t\"./log\": \"./node_modules/webpack/hot/log.js\"\n};\n\n\nf
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => "53cea6ce02f7505145ba"
+/******/ 		__webpack_require__.h = () => "c07211079557387b4a36"
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/modules/modalCallback.js
+++ src/modules/modalCallback.js
@@ -1,22 +1,19 @@
-
-const modalCallback = () => {
+document.addEventListener('click', ({ target }) => {
     const modalOverlay = document.querySelector('.modal-overlay');
     const callback = document.getElementById('callback');
+    const feedback = document.getElementById('feedback');
+    if (target.closest('.callback-btn')) {
+        modalOverlay.style.cssText = 'display:block; opacity:1; transition: all .9s ease 3s;';
+        callback.style.cssText = 'display:block; opacity:1; transform: scale(1) translate(-50%, 0) rotate(0deg); transition: all .9s ease 1s;';
+    }
+    if (target.closest('.modal-overlay') || target.closest('.modal-close')) {
+        modalOverlay.style.cssText = 'display:none; opacity:0; transform:translate(0, 0) rotate(45deg); transition: all .9s ease 3s;';
+        callback.style.cssText = 'display:none; opacity:0; transform:scale(1) translate(0, 0) rotate(45deg); transition: all .9s ease 3s;';
+    }
+})
 
-    document.addEventListener('click', ({ target }) => {
-        if (target.classList.contains('callback-btn')) {
-            modalOverlay.style.display = 'block';
-            callback.style.display = 'block';
-        }
-        if (target.classList.contains('.modal-close')) {
-            modalOverlay.style.display = 'none';
-            callback.style.display = 'none';
-        }
-        console.log(target);
-        
-    })
 
-};
+
 
 
 export default modalCallback;
