7f7af24928690879111cd8d77fd37500d1390cd8
Aleksandr <registr99999@gmail.com>
2021-01-15 17:26:41 +0300

    карточка по валидации формы и AJAX

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@
     import modalCallback from "./modules/modalCallback";
     import smoothScroll from "./modules/smoothScroll";
     import spoilers from "./modules/spoilers";
-
+    import sendForm from "./modules/sendForm";
+    
 
     
     modalCallback();
     smoothScroll();
     spoilers();
+    sendForm();
---
+++ src/modules/sendForm.js
@@ -0,0 +1,90 @@
+
+const sendForm = () => {
+    // обявление переменных
+    const errorMessage = 'Что то пошло не так!';
+    const loadMessage = 'Загрузка... ';
+    const successMessage = 'Спасибо! Мы скоро с Вами свяжемся!';
+    const inputName = document.getElementsByName('fio')[0];
+    const inputTel = document.getElementsByName('tel')[0];
+    const form = document.getElementsByName('form-callback')[0];
+    
+    // валидация полей
+    inputName.addEventListener('input', () => {
+        inputName.value = inputName.value.replace(/[^а-яё\s]/ig, '');
+        if (inputName.value.length < 2) {
+            inputName.setCustomValidity("имя должно быть не меенее 2х символов");
+            inputName.reportValidity();
+        } else {
+            inputName.setCustomValidity("");
+        }
+    })
+    inputTel.setAttribute('maxlength', '12');
+    inputTel.addEventListener('input', () => {
+        inputTel.value = inputTel.value.replace(/[^0-9|\+]/g, '');
+        if (/^\+?(7|8)\d{7,11}$/.test(inputTel.value)) {
+            inputTel.setCustomValidity("");
+        } else if (inputTel.value.length <= 7) {
+            inputTel.setCustomValidity("не менее 7 символов");
+            inputTel.reportValidity();
+        } else {
+            inputTel.setCustomValidity("формат  89991110011");
+            inputTel.reportValidity();
+
+        }
+    })
+    const statusMessage = document.createElement('div');
+    statusMessage.style.cssText = 'font-size: 2rem;';
+     
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        form.appendChild(statusMessage);
+        statusMessage.style.cssText = 'color: #000000;';
+
+        statusMessage.textContent = loadMessage;
+        const formData = new FormData(form);
+        let body = {};
+        formData.forEach((val, key) => {
+            body[key] = val;
+        });
+        postSata(body, () => {
+            statusMessage.textContent = successMessage;
+        }, (error) => {
+            statusMessage.textContent = errorMessage;
+            console.error(error);
+        });
+        let count = 0;
+        const removeMessage = setInterval(() => {
+            count++;
+            console.log(count);
+            if (count >= 5) {
+                clearInterval(removeMessage);
+                statusMessage.textContent = '';
+            }
+        }, 1000);
+        form.reset();
+
+
+    });
+    const postSata = (body) => {
+        return new Promise((resolve, project) => {
+            const request = new XMLHttpRequest();
+            request.addEventListener('readystatechange', () => {
+                if (request.readyState !== 4) {
+                    return;
+                }
+                if (request.status === 200) {
+                    resolve(statusMessage.textContent = successMessage);
+                } else {
+                    project(request.status);
+                }
+            });
+            request.open('POST', 'server.php');
+            request.setRequestHeader('Content-Type', 'application/json');
+            request.send(JSON.stringify(body));
+        })
+
+    }
+}
+
+export default sendForm;
+
