4cfa8543bb6f2090291b8dbefe2d9988c8f95e99
Aleksandr <registr99999@gmail.com>
2021-01-18 12:18:25 +0300

    Добавление бандла в репозиторий

---
+++ dist/main.ce336f1e400bbfc8a4bc.hot-update.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdatediplom"]("main",{
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _modules_modalCallback__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./modules/modalCallback */ \"./src/modules/modalCallback.js\");\n/* harmony import */ var _modules_smoothScroll__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./modules/smoothScroll */ \"./src/modules/smoothScroll.js\");\n/* harmony import */ var _modules_spoilers__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./modules/spoilers */ \"./src/modules/spoilers.js\");\n/* harmony import */ var _modules_sendForm__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./modules/sendForm */ \"./src/modules/sendForm.js\");\n/* harmony import */ var _modules_mainSlider__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./modules/mainSlider */ \"./src/modules/mainSlider.js\");\n/* harmony import */ var _modules_carousel__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ./modules/carousel */ \"./src/modules/carousel.js\");\n\n\n\n\n\n\n\n\n(0,_modules_mainSlider__WEBPACK_IMPORTED_MODULE_4__.default)();\n(0,_modules_modalCallback__WEBPACK_IMPORTED_MODULE_0__.default)();\n(0,_modules_smoothScroll__WEBPACK_IMPORTED_MODULE_1__.default)();\n(0,_modules_spoilers__WEBPACK_IMPORTED_MODULE_2__.default)();\n(0,_modules_sendForm__WEBPACK_IMPORTED_MODULE_3__.default)();\nvar carousel = new _modules_carousel__WEBPACK_IMPORTED_MODULE_5__.default({\n  main: '.services-elements',\n  wrap: '.services-carousel',\n  prev: '.arrow-left',\n  next: '.arrow-right',\n  slidesToShow: 3,\n  infinity: 2\n});\ncarousel.init();\n\n//# sourceURL=webpack://diplom/./src/index.js?");
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ 	"use strict";
+/******/ 
+/******/ 	/* webpack/runtime/getFullHash */
+/******/ 	(() => {
+/******/ 		__webpack_require__.h = () => "f44af9cbd2f3a3244411"
+/******/ 	})();
+/******/ 	
+/******/ }
+);
